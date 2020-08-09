42fa864594fc6ead235b40fee015b53570f7eba3
XxArnavxX <60574509+XxArnavxX@users.noreply.github.com>
2020-08-09 20:22:35 +0530

    Add files via upload

---
+++ screens/booktransaction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class BookTransaction extends React.Component{
+    render(){
+        return(
+            <View>
+                <text>
+                    issue or return
+                </text>
+            </View>
+
+        );
+    }
+}
---
+++ screens/searchscreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <text>
+                    search
+                </text>
+            </View>
+
+        );
+    }
+}
